c71fe881cb90335756845c4786c167306a46da13
Haaget <haaget.cvb@gmail.com>
2025-04-07 18:43:50 +0300

    Виправлені невеліки помилки у коді.

--- gan-mnist-generator.py
+++ gan-mnist-generator.py
@@ -8,7 +8,7 @@ from tensorflow.keras import layers
 X_train = X_train.astype("float32") / 255.0
 X_train = np.expand_dims(X_train, axis=-1)  # (60000, 28, 28, 1)
 
-BUFFER_SIZE = 60000
+BUFFER_SIZE = 10000
 BATCH_SIZE = 128
 LATENT_DIM = 100
 EPOCHS = 500
@@ -59,17 +59,12 @@ cross_entropy = tf.keras.losses.BinaryCrossentropy()
 generator_optimizer = tf.keras.optimizers.Adam(1e-4)
 discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
 
-# Мітки
-real_label = tf.ones((BATCH_SIZE, 1))
-fake_label = tf.zeros((BATCH_SIZE, 1))
-
 # Пайплайн даних
 dataset = tf.data.Dataset.from_tensor_slices(X_train).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
 
 # Тренувальний цикл
-@tf.function
-def train_step(images):
-    noise = tf.random.normal([BATCH_SIZE, LATENT_DIM])
+def train_step(images, epoch):
+    noise = tf.random.normal([tf.shape(images)[0], LATENT_DIM])  # Используем размер текущего батча
 
     # Тренування дискримінатора
     with tf.GradientTape() as disc_tape:
@@ -78,17 +73,20 @@ def train_step(images):
         real_output = discriminator(images, training=True)
         fake_output = discriminator(generated_images, training=True)
 
+        real_label = tf.ones_like(real_output)  # Динамические метки
+        fake_label = tf.zeros_like(fake_output)  # Динамические метки
+
         disc_loss = cross_entropy(real_label, real_output) + cross_entropy(fake_label, fake_output)
 
     grads_disc = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
     discriminator_optimizer.apply_gradients(zip(grads_disc, discriminator.trainable_variables))
 
     # Тренування генератора
-    noise = tf.random.normal([BATCH_SIZE, LATENT_DIM])
     with tf.GradientTape() as gen_tape:
         generated_images = generator(noise, training=True)
         fake_output = discriminator(generated_images, training=True)
-        gen_loss = cross_entropy(real_label, fake_output)
+
+        gen_loss = cross_entropy(real_label, fake_output)  # Динамическая метка для генератора
 
     grads_gen = gen_tape.gradient(gen_loss, generator.trainable_variables)
     generator_optimizer.apply_gradients(zip(grads_gen, generator.trainable_variables))
@@ -100,7 +98,11 @@ def train_step(images):
     # Рахуємо точність генератора (як кількість "реальних" згенерованих зображень)
     gen_accuracy = tf.reduce_mean(tf.cast(fake_output > 0.5, tf.float32))
 
-    return gen_loss, disc_loss, disc_accuracy, gen_accuracy
+    print(f"Epoch {epoch}, Generator Loss: {gen_loss:.4f}, Discriminator Loss: {disc_loss:.4f}, "
+          f"Discriminator Accuracy: {disc_accuracy:.4f}, Generator Accuracy: {gen_accuracy:.4f}")
+    epoch += 1
+
+    return gen_loss, disc_loss, disc_accuracy, gen_accuracy, epoch
 
 # Візуалізація
 def generate_and_save_images(model, epoch):
@@ -119,11 +121,8 @@ def generate_and_save_images(model, epoch):
 
 # Навчання
 for epoch in range(1, EPOCHS+1):
-    for image_batch in dataset:
-        gen_loss, disc_loss, disc_accuracy, gen_accuracy = train_step(image_batch)
-
-    print(f"Epoch {epoch}, Generator Loss: {gen_loss:.4f}, Discriminator Loss: {disc_loss:.4f}, "
-          f"Discriminator Accuracy: {disc_accuracy:.4f}, Generator Accuracy: {gen_accuracy:.4f}")
+    for images in dataset:
+        gen_loss, disc_loss, disc_accuracy, gen_accuracy, epoch = train_step(images, epoch)
 
     if epoch % SAVE_INTERVAL == 0 or epoch == 1:
         generate_and_save_images(generator, epoch)
